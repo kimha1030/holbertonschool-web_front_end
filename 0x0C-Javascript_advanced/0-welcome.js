94511e31d2bb362f6555db54e3265c2aef02203d
kimha1030 <1478@holbertonschool.com>
2021-02-20 22:05:09 -0500

    Lexical scoping and welcome message

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+  let fullName = firstName + " " + lastName;
+  function displayFullName() {
+    alert("Welcome " + fullName + "!");
+  }
+  displayFullName();
+}
